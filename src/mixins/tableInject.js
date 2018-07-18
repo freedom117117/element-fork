beb0bf5383101a250d6919316742e1ad1f3f12dd
Deng <user@ipn-37-220-81-64.artem-catv.ru>
2018-07-18 17:31:31 +0800

    fix table bug

--- src/mixins/tableInject.js
+++ src/mixins/tableInject.js
@@ -11,19 +11,31 @@ export default {
   },
   computed: {
     disabledForTable() {
-      return (this.elTable || {}).disabled || (this.elTable !== '' && this.elTable.tableWithStatus && this.tableRowIndex !== '' && this.elTable.data[this.tableRowIndex].dataStatus === 'delete') ;
+      return (this.elTable || {}).disabled || (this.elTable !== '' && this.elTable.tableWithStatus && this.tableRowIndex !== '' && this.elTable.data[this.tableRowIndex].dataStatus === 'delete');
     }
   },
   watch: {
     'elTable.data.length'(val, oldValue) {
       if (this.elTable) {
-        this.tableRowIndex = Array.prototype.slice.call(this.$el.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.el-table__row')).indexOf(this.$el.parentNode.parentNode.parentNode);
+        this.tableRowIndex = Array.prototype.slice.call(this.getTableLine(this.$el).parentNode.querySelectorAll('.el-table__row')).indexOf(this.getTableLine(this.$el));
+        if (this.tableRowIndex < 0) {
+          this.tableRowIndex = 0;
+        }
+      }
+    }
+  },
+  methods: {
+    getTableLine(DOM) {
+      if (DOM.tagName === 'TR' && DOM.getAttribute('class').indexOf('el-table__row') !== -1) {
+        return DOM;
+      } else {
+        return this.getTableLine(DOM.parentNode);
       }
     }
   },
   mounted() {
     if (this.elTable) {
-      this.tableRowIndex = Array.prototype.slice.call(this.$el.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.el-table__row')).indexOf(this.$el.parentNode.parentNode.parentNode);
+      this.tableRowIndex = Array.prototype.slice.call(this.getTableLine(this.$el).parentNode.querySelectorAll('.el-table__row')).indexOf(this.getTableLine(this.$el));
     }
   }
 };
